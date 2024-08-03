75261530dc9bd3a6b09ec029678dbd5a9b1cadf7
nagaraju Vuppala <vpp.nagaraju1234@gmail.com>
2024-08-03 14:20:41 +0530

    other files commit

---
+++ ChangeLog.md
@@ -0,0 +1,12 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
+
+## [Unreleased]
+
+### [0.1] - 05-08-2019...... V Nagaraju <vpp.nagaraju1234@gmail.com>
+	* Version 6.3
+### Updates
+
+- Cards to view websites.
---
+++ index.php
@@ -0,0 +1,199 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="bootstrap-4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
+		<title>PLURAL | TOOLS</title>
+	</head>
+	<body>
+		<h1></h1>
+		<div class="app" data-finder-wrapper data-finder-scroll-offset="175">
+		<button class="finder-activator" data-finder-activator>
+	  Search
+	</button>
+		<div class="container container-fluid">
+			<div class="row content" data-finder-content>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Tamil Parser</h5>
+						<div class="card-body">
+							<h5 class="card-title"> Tamil Parser in UD format</h5>
+							<p class="card-text">POS, Morph, Dependency Relation, Graph View</p>
+							<a href="https://plural.iiit.ac.in/tamilparser" target="_blank" class="btn btn-primary">Plural</a>
+							<!-- <a href="#" target="_blank" class="btn btn-primary">Local</a> -->
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Transliteration</h5>
+						<div class="card-body">
+							<h5 class="card-title"> Indian Languages Transliteration</h5>
+							<p class="card-text">Tamil, Telugu, Hindi, Malayalam</p>
+							<a href="https://plural.iiit.ac.in/IL-UTF2WX/" target="_blank" class="btn btn-primary">Plural</a>
+							<a href="http://10.2.6.188/utfwx" target="_blank" class="btn btn-primary">Local</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">CONLL-U Annotator</h5>
+						<div class="card-body">
+							<h5 class="card-title"> For building Parser</h5>
+							<p class="card-text">Tamil, Telugu</p>
+							<a href="https://plural.iiit.ac.in/conllu-annotator" target="_blank" class="btn btn-primary">Plural</a>
+							<a href="http://10.2.6.188:300" target="_blank" class="btn btn-primary">Local</a>
+						</div>
+					</div>
+				</div>
+				<hr>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Morph Analyzer</h5>
+						<div class="card-body">
+							<h5 class="card-title"> Indian Languages Morph</h5>
+							<p class="card-text">Tamil, Telugu</p>
+							<a href="https://plural.iiit.ac.in/morphAnalyzer" target="_blank" class="btn btn-primary">Plural</a>
+							<a href="http://10.2.6.188/morphAnalyzer" target="_blank" class="btn btn-primary">Local</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Discourse-Annotator</h5>
+						<div class="card-body">
+							<h5 class="card-title"> For building discourse data</h5>
+							<p class="card-text">Tamil, Telugu</p>
+							<a href="https://plural.iiit.ac.in/discourse" target="_blank" class="btn btn-primary">Plural</a>
+							<a href="http://10.2.6.188/discourse" target="_blank" class="btn btn-primary">Local</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">CONLL-U Validator</h5>
+						<div class="card-body">
+							<h5 class="card-title"> For Parser 10 Column CONLL-U Validation</h5>
+							<p class="card-text">Tamil, Telugu</p>
+							<a href="https://plural.iiit.ac.in/" target="_blank" class="btn btn-primary">Plural</a>
+							<a href="http://10.2.6.188/conllu-validate" target="_blank" class="btn btn-primary">Local</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="card">
+						<h5 class="card-header">Annotation Stats</h5>
+						<div class="card-body">
+							<h5 class="card-title"> Each column annotation stats comparision</h5>
+							<p class="card-text">1 File or 2 File Upload</p>
+							<a href="https://plural.iiit.ac.in/" target="_blank" class="btn btn-primary">Plural</a>
+							<a href="http://10.2.6.188/annotate-stats" target="_blank" class="btn btn-primary">Local</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Content here -->
+		</div>
+	</div>
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"
+	        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+	        crossorigin="anonymous"
+	></script>
+		<!--<script src="jQuery/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
+		<script src="jQuery/jquery.highlight.js"></script>
+		<script src="jQuery/jquery.scrollto.js"></script>
+		<script src="jQuery/jquery.finder.js"></script>
+		<script src="bootstrap-4.0.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
+		<link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+            integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+            crossorigin="anonymous"
+        />
+
+	</body>
+	<style>
+	.finder {
+	position: fixed;
+	top: -50px;
+	right: 0;
+	left: 0;
+	z-index: 999;
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: -webkit-flex;
+	display: flex;
+	height: 50px;
+	background: #fff;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 14px;
+	border-bottom: 1px solid #e5e5e5;
+	box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+	padding: 5px;
+	transition: top 0.15s ease-out;
+}
+
+.finder.active {
+	top: 0;
+}
+
+.finder-input {
+	flex-grow: 1;
+	width: 0;
+	height: 40px;
+	border: none;
+	padding: 8px;
+	outline: none !important;
+}
+
+.finder-input.not-found {
+	color: #e7353f;
+}
+
+.btn-finder {
+	width: 40px;
+	height: 40px;
+	text-align: center;
+	padding: 0;
+	color: #000;
+}
+
+.finder-input,
+.btn-finder:not(:last-child) {
+	margin-right: 5px;
+}
+
+.finder-count {
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: -webkit-flex;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	color: #888;
+	border-right: 1px solid #e5e5e5;
+	margin-right: 5px;
+	padding-right: 20px;
+	white-space: nowrap;
+}
+
+.btn-finder,
+.finder-count {
+	flex-shrink: 0;
+	flex-grow: 0;
+}
+
+.highlight {
+	background: #ff8;
+}
+
+.highlight.active {
+	background: #ffd71b;
+}	
+	</style>
+</html>
